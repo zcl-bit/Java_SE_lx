a8a74466b834d1f5a81187bcb40b9f0e5d318ed8
zcl <zcl200471@gmail.com>
2021-12-18 15:25:06 +0800

    2021/12/18 新增方法以及递归练习项目

--- src/Recursion/Test01.java
+++ src/Recursion/Test01.java
@@ -1,15 +1,20 @@
 package Recursion;
-/*
-* */
+/**
+ * 定义递归要有结束的条件
+ *
+ * */
 public class Test01 {
+    public static int num = 10;
     public static void a(){
-        System.out.println("Test01.a");
-        a();
+        num--;
+        System.out.println("Test01.a" + num);
+        if (num > 0){
+            a();
+        }
     }
 
     public static void b(){
-        System.out.println("Test01.a");
-
+        System.out.println("Test01.b");
     }
 
     public static void main(String[] args) {
