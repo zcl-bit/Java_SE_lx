709aa2f1510502775f8e1c5799de45ebcf5d4567
zcl <zcl200471@gmail.com>
2021-12-25 16:49:03 +0800

    2021/12/25 oop新增Final

---
+++ oop/final/src/FinalDemo.java
@@ -0,0 +1,17 @@
+/**
+ * @author zcl
+ * @date 2021/12/25 16:44
+ */
+public final class FinalDemo {
+
+    public final void test(){
+        System.out.println("final test");
+
+    }
+    public static final int age= 11;
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ oop/final/src/FinalSubClass.java
@@ -0,0 +1,11 @@
+/**
+ * @author zcl
+ * @date 2021/12/25 16:45
+ */
+public class FinalSubClass extends FinalDemo{
+    @Override
+    public final void test(){
+
+    }
+
+}
