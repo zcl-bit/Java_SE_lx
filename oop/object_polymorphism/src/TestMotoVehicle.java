b72813ffc0baaeac33062bf5ebca4c6f7c1daeba
zcl <zcl200471@gmail.com>
2021-12-26 10:01:06 +0800

    2021/12/26 oop新增Polymorphism

--- oop/object_polymorphism/src/TestMotoVehicle.java
+++ oop/object_polymorphism/src/TestMotoVehicle.java
@@ -10,13 +10,19 @@ public class TestMotoVehicle {
 //                return 0;
 //            }
 //        };抽象类中虽然可以进行构造方法的继承但是不允许实例化
-        Car car = new Car(1,"宝马","1");
-        System.out.println("租金是："+car.calcRent(5));
-        Bus bus = new Bus(2,"金龙",20);
-        System.out.println("租金是："+bus.calcRent(5));
-
-
-
-
+//        Car car = new Car(1,"宝马","1");
+//        System.out.println("租金是："+car.calcRent(5));
+//        Bus bus = new Bus(2,"金龙",20);
+//        System.out.println("租金是："+bus.calcRent(5));
+        MotoVehicle[] motoVehicles = new MotoVehicle[4];
+        motoVehicles[0] = new Car(1, "宝马", "1");
+        motoVehicles[1] = new Car(1, "宝马", "1");
+        motoVehicles[2] = new Car(2, "别克", "2");
+        motoVehicles[3] = new Bus(3, "金龙", 34);
+        int totalMoney = 0;
+        for (int i = 0; i < motoVehicles.length; i++) {
+            totalMoney += motoVehicles[i].calcRent(5);
+        }
+        System.out.println("总租金是：" + totalMoney);
     }
 }
