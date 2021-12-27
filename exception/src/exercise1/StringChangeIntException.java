1d101a0c278ed1629492be56dce2ccfd4675a1c1
zcl <zcl200471@gmail.com>
2021-12-27 11:50:11 +0800

    2021/12/27 新增异常

--- exception/src/exercise1/StringChangeIntException.java
+++ exception/src/exercise1/StringChangeIntException.java
@@ -10,6 +10,9 @@ public class StringChangeIntException {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         try{
+            System.out.println("请输入要进行转换的字符");
+            String input = in.next();
+            System.out.println(changeInt(input));
 
         }catch (NumberFormatException e){
             System.out.println("字符类型数据无法转换成整型数据");
@@ -19,9 +22,9 @@ public class StringChangeIntException {
             e.printStackTrace();
         }
     }
-    public int changeInt(String name){
+    public static int changeInt(String name){
+//      不兼容无法转换
 //        int a = (int)name;
-//        return a;
         return 0;
     }
 }
