b8c212181c93dd19106ca7d929d0df6fc79c545d
zcl <zcl200471@gmail.com>
2022-01-06 11:23:13 +0800

    2022/1/6 新增电话本项目

--- telephone_book/src/Operate.java
+++ telephone_book/src/Operate.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -7,128 +8,357 @@ import java.util.List;
  * @create: 2022/1/6 10:27
  * 核心业务类
  */
-public class Operate {
-    private List<Person> list ;
-    public Operate(){
+public class Operate{
+    private List<Person> list;
+
+    public Operate() {
         this.list = new ArrayList<>();
     }
+
     /**
      * 用户添加记录业务逻辑控制
      */
-    public void addLogic(){
-
+    public void addLogic() {
+        Menu menu = new Menu();
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        while (true) {
+            menu.addMenu();
+            int item = telNoteRegex.menuItemValidate(1, 3);
+            switch (item) {
+                case 1:
+                    this.addOperation();
+                    break;
+                case 2:
+                    this.showAll();
+                    break;
+                case 3:
+                    return;
+                default:
+                    System.out.println("输入错误");
+                    continue;
+            }
+        }
     }
 
     /**
      * 用户查询记录业务逻辑控制
      */
-    public void searchLogic(){
-
+    public void searchLogic() {
+        Menu menu = new Menu();
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        while (true) {
+            menu.searchMenu();
+            int item = telNoteRegex.menuItemValidate(1, 7);
+            switch (item) {
+                case 1:
+                    this.searchByName();
+                    break;
+                case 2:
+                    this.searchByAge();
+                    break;
+                case 3:
+                    this.searchBySex();
+                    break;
+                case 4:
+                    this.searchByTelNum();
+                    break;
+                case 5:
+                    this.searchByAdd();
+                    break;
+                case 6:
+                    this.showAll();
+                    break;
+                case 7:
+                    return;
+            }
+        }
     }
 
     /**
      * 修改记录业务逻辑控制
      */
-    public void modifyLogic(){
-
+    public void modifyLogic() {
+        Menu menu = new Menu();
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        while (true) {
+            menu.modifyMenu();
+            int item = telNoteRegex.menuItemValidate(1, 3);
+            switch (item) {
+                case 1:
+                    this.showAll();
+                    break;
+                case 2:
+                    this.modifyOperation();
+                    break;
+                case 3:
+                    return;
+            }
+        }
     }
 
     /**
      * 删除记录业务逻辑控制
      */
-    public void deleteLogic(){
-
+    public void deleteLogic() {
+        Menu menu = new Menu();
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        while (true) {
+            menu.deleteMenu();
+            int item = telNoteRegex.menuItemValidate(1, 4);
+            switch (item) {
+                case 1:
+                    this.showAll();
+                    break;
+                case 2:
+                    this.deleteOperation();
+                    break;
+                case 3:
+                    this.deleteAllOperation();
+                    break;
+                case 4:
+                    return;
+            }
+        }
     }
 
     /**
-     *  排序记录业务逻辑控制
+     * 排序记录业务逻辑控制
      */
-    public void orderLogic(){
-
+    public void orderLogic() {
+        Menu menu = new Menu();
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        while (true) {
+            menu.orderMenu();
+            int item = telNoteRegex.menuItemValidate(1, 5);
+            switch (item) {
+                case 1:
+                    this.orderName();
+                    break;
+                case 2:
+                    this.orderAge();
+                    break;
+                case 3:
+                    this.orderSex();
+                    break;
+                case 4:
+                    this.showAll();
+                    break;
+                case 5:
+                    return;
+            }
+        }
     }
 
     /**
      * 添加新记录信息
      */
-    public void addOperation(){
-
+    public void addOperation() {
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        String name = telNoteRegex.nameValidate();
+        String age = telNoteRegex.ageValidate();
+        String sex = telNoteRegex.sexValidate();
+        String telNum = telNoteRegex.telNumValidate();
+        String address = telNoteRegex.addressValidate();
+        Person person = new Person(name, age, sex, telNum, address);
+        this.list.add(person);
+        person.setId(this.list.size());
     }
 
     /**
      * 查询全部记录
      */
-    public void showAll(){
-
+    public void showAll() {
+        if (this.list.size() == 0) {
+            System.out.println("没有任何记录");
+            return;
+        }
+        for (int i = 0; i < this.list.size(); i++) {
+            System.out.println(this.list.get(i));
+        }
     }
+
     /**
      * 按姓名查询记录
      */
-    public void searchByName(){
-
+    public void searchByName() {
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        String name = telNoteRegex.nameValidate();
+        boolean flag = true;
+        for (int i = 0; i < this.list.size(); i++) {
+            if (name.equals(this.list.get(i).getName())) {
+                System.out.println(this.list.get(i));
+                flag = false;
+            }
+        }
+        if (flag) {
+            System.out.println("没有此人记录");
+        }
     }
+
     /**
      * 按年龄查询记录
      */
-    public void searchByAge(){
-
+    public void searchByAge() {
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        String age = telNoteRegex.ageValidate();
+        boolean flag = true;
+        for (int i = 0; i < this.list.size(); i++) {
+            if (age.equals(this.list.get(i).getAge())) {
+                System.out.println(this.list.get(i));
+                flag = false;
+            }
+        }
+        if (flag) {
+            System.out.println("没有此人记录");
+        }
     }
 
     /**
      * 按性别查询记录
      */
-    public void searchBySex(){
-
+    public void searchBySex() {
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        String sex = telNoteRegex.sexValidate();
+        boolean flag = true;
+        for (int i = 0; i < this.list.size(); i++) {
+            if (sex.equalsIgnoreCase(this.list.get(i).getSex())) {
+                System.out.println(this.list.get(i));
+                flag = false;
+            }
+        }
+        if (flag) {
+            System.out.println("没有此人记录");
+        }
     }
+
     /**
      * 按电话号码查询记录
      */
-    public void searchByTelNum(){
-
+    public void searchByTelNum() {
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        String telNum = telNoteRegex.telNumValidate();
+        boolean flag = true;
+        for (int i = 0; i < this.list.size(); i++) {
+            if (telNum.equals(this.list.get(i).getTelNum())) {
+                System.out.println(this.list.get(i));
+                flag = false;
+            }
+        }
+        if (flag) {
+            System.out.println("没有此人记录");
+        }
     }
+
     /**
      * 按地址查询记录
      */
-    public void searchByAdd(){
-
+    public void searchByAdd() {
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        String address = telNoteRegex.addressValidate();
+        boolean flag = true;
+        for (int i = 0; i < this.list.size(); i++) {
+            if (address.equals(this.list.get(i).getAddress())) {
+                System.out.println(this.list.get(i));
+                flag = false;
+            }
+        }
+        if (flag) {
+            System.out.println("没有此人记录");
+        }
     }
+
     /**
      * 修改指定记录
      */
-    public void modifyOperation(){
-
+    public void modifyOperation() {
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        Menu menu = new Menu();
+        //对被修改的记录的序号进行验证，可以使用对菜单项验证的方法来完成
+        System.out.println("请输入记录的序号");
+        int itemNum = telNoteRegex.menuItemValidate(1, this.list.size());
+        menu.subModifyMenu();
+        int menuItem = telNoteRegex.menuItemValidate(1, 6);
+        switch (menuItem) {
+            case 1:
+                String name = telNoteRegex.nameValidate();
+                (this.list.get(itemNum - 1)).setName(name);
+                break;
+            case 2:
+                String age = telNoteRegex.ageValidate();
+                (this.list.get(itemNum - 1)).setAge(age);
+                break;
+            case 3:
+                String sex = telNoteRegex.sexValidate();
+                (this.list.get(itemNum - 1)).setSex(sex);
+                break;
+            case 4:
+                String telNum = telNoteRegex.telNumValidate();
+                (this.list.get(itemNum - 1)).setTelNum(telNum);
+                break;
+            case 5:
+                String address = telNoteRegex.addressValidate();
+                (this.list.get(itemNum - 1)).setAddress(address);
+                break;
+            case 6:
+                return;
+        }
     }
+
     /**
      * 删除指定记录
      */
-    public void deleteOperation(){
-
+    public void deleteOperation() {
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+        System.out.println("请输入记录序号");
+        int itemNum = telNoteRegex.menuItemValidate(1,this.list.size());
+        this.list.remove(itemNum -1);
+        //重新为记录设置新的序号
+        for(int i=0;i<this.list.size();i++){
+            (this.list.get(i)).setId(i+1);
+        }
+        System.out.println("删除成功！请继续操作！");
     }
+
     /**
      * 删除全部记录
      */
-    public void deleteAllOperation(){
-
+    public void deleteAllOperation() {
+        this.list.clear();
+        System.out.println("电话中的记录已清空，请继续操作！");
     }
 
     /**
      * 按用户姓名排序记录
      */
     public void orderName(){
-
+        Collections.sort(this.list,new OrderByName());
+        for(int i=0;i<this.list.size();i++){
+            (this.list.get(i)).setId(i+1);
+        }
     }
+
     /**
      * 按用户年龄排序记录
      */
-    public void orderAge(){
+    public void orderAge() {
+        Collections.sort(this.list,new OrderByAge());
+        for(int i=0;i<this.list.size();i++){
+            (this.list.get(i)).setId(i+1);
+        }
 
     }
 
     /**
      * 按用户性别排序记录
      */
-    public void orderSex(){
-
+    public void orderSex() {
+        Collections.sort(this.list,new OrderBySex());
+        for(int i=0;i<this.list.size();i++){
+            (this.list.get(i)).setId(i+1);
+        }
     }
+
+
 
     /**
      * 按姓名排序的比较器
@@ -137,27 +367,27 @@ public class Operate {
 
         @Override
         public int compare(Person o1, Person o2) {
-            return 0;
+            return o1.getName().compareTo(o2.getName());
         }
     }
 
     /**
      * 按年龄排序的比较器
      */
-    class OrderByAge implements Comparator<Person>{
+    class OrderByAge implements Comparator<Person> {
         @Override
         public int compare(Person o1, Person o2) {
-            return 0;
+            return o1.getAge().compareTo(o2.getAge());
         }
     }
 
     /**
      * 按性别排序的比较器
      */
-    class OrderBySex implements Comparator<Person>{
+    class OrderBySex implements Comparator<Person> {
         @Override
         public int compare(Person o1, Person o2) {
-            return 0;
+            return o1.getSex().compareTo(o2.getSex());
         }
     }
 }
--- telephone_book/src/TelNoteRegex.java
+++ telephone_book/src/TelNoteRegex.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @author: zcl
  * @create: 2022/1/6 10:27
@@ -11,47 +13,145 @@ public class TelNoteRegex {
      * @return
      */
     public int menuItemValidate (int min, int max ){
-        return 0;
+        //定义验证菜单项的正则表达式
+        String regex = "[1-9]{1}";
+        //创建键盘输入对象
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("请输入正确数字，最小是："+min+"\t"+"最大是："+max);
+            String input = scanner.nextLine();
+            if(input.matches(regex)){
+                int inputNum = Integer.parseInt(input);
+                if(inputNum >= min && inputNum <= max){
+                    return inputNum;
+                }else{
+                    System.out.println("您输入的菜单项不符，请重新输入！");
+                }
+            }else{
+                System.out.println("输入数字错误，请检查！");
+            }
+        }
     }
 
     /**
      *  对用户输入姓名的验证
+     *  字母可以是大写或者小写，长度1-10之间的
      * @return
      */
     public String nameValidate ( ){
-        return null;
+        //验证姓名的正则表达式
+        String regex = "[a-zA-Z]{1,10}";
+        //创建Scanner对象
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("请输入姓名： 格式为1-10之间的大写或小写字母");
+            String input = scanner.nextLine();
+            if(input.matches(regex)){
+                return input;
+            }else{
+                System.out.println("您当前输入的姓名有误，请重新输入！");
+            }
+        }
     }
 
     /**
      * 对用户输入年龄的验证
+     * 年龄的格式要求：10-99之间的
      * @return
      */
     public String ageValidate ( ){
-        return null;
+        //对年龄校验的正则表达式
+        String regex = "[1-9]{1}[0-9]{1}";
+        //创建Scanner对象
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("请输入年龄：格式为10-99之间");
+            String input = scanner.nextLine();
+            if(input.matches(regex)){
+                return input;
+            }else{
+                System.out.println("您输入的年龄格式有误，请重新输入！");
+            }
+        }
     }
 
     /**
      *  对用户输入性别的验证
+     *  性别的输入要求：男（m 或 M） 女（f 或 F）
      * @return
      */
     public String sexValidate ( ){
-        return null;
+        //对性别验证的正则表达式
+        String regex ="[m|M|f|F]{1}";
+        //创建Scanner对象
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("输入性别：(男 m or M) (女 f or F)");
+            String input = scanner.nextLine();
+            if(input.matches(regex)){
+                return input;
+            }else{
+                System.out.println("输入性别格式有误，请重新输入！");
+            }
+        }
     }
 
     /**
      * 对用户输入电话号码的验证
+     * 电话号码要求：允许带有区号的座机号，允许手机号
      * @return
      */
     public String telNumValidate ( ){
-        return null;
+        //对电话号码验证的正则表达式
+        String regex = "(\\d{3,4}-\\d{7,8})|([1]{1}\\d{10})";
+        //创建Scanner对象
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("请输入电话号码：可以是座机号或者是手机号。");
+            String input = scanner.nextLine();
+            if(input.matches(regex)){
+                return input;
+            }else{
+                System.out.println("输入的电话号码有误，请重新输入！");
+            }
+        }
     }
 
     /**
      * 对用户输入地址的验证
+     * 地址格式要求：字母或者数字，长度1,50
      * @return
      */
     public String addressValidate ( ){
-        return null;
+        //对地址验证的正则表达式
+        String regex = "\\w{1,50}";
+        //创建Scanner对象
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("请输入地址：格式为字母或数字，长度为1,50");
+            String input = scanner.nextLine();
+            if(input.matches(regex)){
+                return input;
+            }else{
+                System.out.println("您输入的地址格式有误，请重新输入。");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TelNoteRegex telNoteRegex = new TelNoteRegex();
+/*        int menuItem = telNoteRegex.menuItemValidate(1,6);
+        System.out.println(menuItem);*/
+/*        String name = telNoteRegex.nameValidate();
+        System.out.println(name);*/
+/*        String telNumValidate = telNoteRegex.telNumValidate();
+        System.out.println(telNumValidate);*/
+/*        String ageValidate = telNoteRegex.ageValidate();
+        System.out.println(ageValidate);*/
+/*        String sexValidate = telNoteRegex.sexValidate();
+        System.out.println(sexValidate);*/
+/*        String addressValidate = telNoteRegex.addressValidate();
+        System.out.println(addressValidate);*/
     }
 
 
